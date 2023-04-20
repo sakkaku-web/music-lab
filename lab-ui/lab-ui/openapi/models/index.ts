bd3c5b81ff7f5cf19b1692a2b2d37f05e66801a3
myin <minyin142p@gmail.com>
2023-04-20 11:33:39 +0200

    add openapi

---
+++ lab-ui/lab-ui/openapi/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './MusicErrorResponse';
+export * from './MusicItemDto';
+export * from './MusicListResponse';
+export * from './MusicTagBody';
+export * from './MusicUnTagBody';
+export * from './TagDto';
+export * from './UnprocessableEntity';
